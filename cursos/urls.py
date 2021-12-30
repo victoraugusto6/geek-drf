73c0a3a0b0359c45f580d5a2a1da69cec5a45a7a
Victor Augusto <victor.augusto.soliveira@gmail.com>
2021-12-30 02:30:10 -0300

    overwrite generics methods

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,6 +4,9 @@ from .views import AvaliacaoApiView, CursoApiView, CursosApiView, AvaliacoesApiV
 urlpatterns = [
     path('cursos/', CursosApiView.as_view(), name='cursos'),
     path('cursos/<int:pk>/', CursoApiView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesApiView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoApiView.as_view(), name='curso_avaliacao'),
+
     path('avaliacoes/', AvaliacoesApiView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:pk>/', AvaliacaoApiView.as_view(), name='avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoApiView.as_view(), name='avaliacao'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
@@ -17,7 +19,18 @@ class AvaliacoesApiView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 
 class AvaliacaoApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(), curso_id=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
