4cdc3501b200f4ffa8b12db0657bacb7ddaccc46
Victor Augusto <victor.augusto.soliveira@gmail.com>
2022-01-25 19:21:41 -0300

    customize validation

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -19,6 +19,12 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'ativo'
         )
 
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):
+            return valor
+        else:
+            raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5.')
+
 
 class CursoSerializer(serializers.ModelSerializer):
     # Nested Relationship
